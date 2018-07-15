61951c4e359291d91ca69d1fe28b74eaba190eaf
jser.li <jser.li@circcus.com>
2018-07-15 15:35:58 +0800

    增加请求压缩中间件

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 
 const express = require('express')
+const compression = require('compression')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
@@ -9,6 +10,8 @@ app.set('port', port)
 
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
+
+app.use(compression())
 
 async function start() {
   const nuxt = new Nuxt(config)
